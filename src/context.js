1ea67344744a046634f5d14d450f8ac9660b864c
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-23 16:51:20 +0545

    product listing img with Links

--- src/context.js
+++ src/context.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
-
+import { storeProducts, detailProduct } from "./data";
+// Context object created with create Context comes with Provider and Consumer
+// react Component
 const ProductContext = React.createContext();
-//Provider
-
-//Consumer
+//const ProductProvider = ProductContext.provider;
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+
+  handleDetail = () => {
+    console.log("hello from detail");
+  };
+
+  addToCart = () => {
+    console.log("hello from add to cart");
+  };
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
