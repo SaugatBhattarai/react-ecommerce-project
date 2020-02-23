55460cb9e150e4d2b2a3e56a02f84acacea37808
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-23 13:15:40 +0545

    context api consumer producer added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
   state = {
     products: storeProducts
@@ -11,8 +12,14 @@ export default class ProductList extends Component {
       <React.Fragment>
         <div className="py-5">
           <div className="container">
-            <Title name="our" title="products for jasmina" />
-            <div className="row">{/* saugat */}</div>
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {v => {
+                  return <h1>{v}</h1>;
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
