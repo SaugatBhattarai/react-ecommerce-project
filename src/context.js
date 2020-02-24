9501020cd85b7ccdd257a9e80495f85e62b3c510
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-24 15:06:51 +0545

    nested object reference issue fixed

--- src/context.js
+++ src/context.js
@@ -7,8 +7,24 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
+  };
+
+  componentDidMount() {
+    this.storeProducts();
+  }
+
+  storeProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+
+    this.setState(() => {
+      return { products: tempProducts };
+    });
   };
 
   handleDetail = () => {
