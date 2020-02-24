1def2818701dbbdab2b345b954b675b25f5b67bb
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-24 11:46:05 +0545

    price and title added

--- src/components/Product.js
+++ src/components/Product.js
@@ -32,10 +32,21 @@ export default class Product extends Component {
               )}
             </button>
           </div>
+          {/* {card footer} */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">${price}</span>
+            </h5>
+          </div>
         </div>
       </ProductWrapper>
     );
   }
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+  }
+`;
