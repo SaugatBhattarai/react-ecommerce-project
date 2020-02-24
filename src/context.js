574ad7069c6204a29a289498c03f2512f0b2685c
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-24 13:03:02 +0545

    added tester method for reference copypaste

--- src/context.js
+++ src/context.js
@@ -18,6 +18,27 @@ class ProductProvider extends Component {
   addToCart = () => {
     console.log("hello from add to cart");
   };
+
+  // tester = () => {
+  //   console.log("State Products: ", this.state.products[0].inCart);
+  //   console.log("Data products: ", storeProducts[0].inCart);
+
+  //   const tempProducts = [...this.state.products];
+  //   tempProducts[0].inCart = true;
+
+  //   this.setState(
+  //     () => {
+  //       return { products: tempProducts };
+  //     },
+  //     () => {
+  //       console.log(
+  //         "After Changes - State Products: ",
+  //         this.state.products[0].inCart
+  //       );
+  //       console.log("After Changes - Data products: ", storeProducts[0].inCart);
+  //     }
+  //   );
+  // };
   render() {
     return (
       <ProductContext.Provider
@@ -27,6 +48,7 @@ class ProductProvider extends Component {
           addToCart: this.addToCart
         }}
       >
+        {/* <button onClick={this.tester}>click me</button> */}
         {this.props.children}
       </ProductContext.Provider>
     );
